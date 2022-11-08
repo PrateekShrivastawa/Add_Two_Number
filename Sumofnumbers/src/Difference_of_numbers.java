216f288e2387107cafba4eea2ec038f2a4fcf70b
Prateek Shrivastawa <prateek.shrivastawa_cs18@gla.ac.in>
2022-11-08 19:32:19 +0530

    Revert "Update Main With Difference Branch"

--- Sumofnumbers/src/Difference_of_numbers.java
+++
@@ -1,9 +0,0 @@
-public class Difference_of_numbers {
-    public static void main(String[] args) {
-        System.out.println(difference(13,12));
-        System.out.println(difference(15,10));
-    }
-    public static int difference(int a,int b){
-        return a-b;
-    }
-}
