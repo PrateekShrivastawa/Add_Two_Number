d05700d8b7d4264ecb9e1aed7cbd7399cf45c871
Prateek Shrivastawa <prateek.shrivastawa_cs18@gla.ac.in>
2022-11-08 19:02:34 +0530

    Sum of Two Numbers Done

---
+++ Sumofnumbers/src/Sum_of_numbers.java
@@ -0,0 +1,8 @@
+public class Sum_of_numbers {
+    public static void main(String[] args) {
+        System.out.println(sum(12,13));
+    }
+    public static int sum(int a,int b){
+        return a+b;
+    }
+}
