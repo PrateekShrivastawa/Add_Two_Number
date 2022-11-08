db031771f63ee67652c8b119b722ef220518f849
Prateek Shrivastawa <prateek.shrivastawa_cs18@gla.ac.in>
2022-11-08 19:06:00 +0530

    Difference of Two Numbers Done

---
+++ Sumofnumbers/src/Difference_of_numbers.java
@@ -0,0 +1,8 @@
+public class Difference_of_numbers {
+    public static void main(String[] args) {
+        System.out.println(difference(13,12));
+    }
+    public static int difference(int a,int b){
+        return a-b;
+    }
+}
